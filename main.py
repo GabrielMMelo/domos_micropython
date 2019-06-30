8290bd8c48d6c24ceec01180e35428d33b7f2827
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-06-30 18:43:53 -0300

    Two-way communication working well
    Both physical and logical switch are working pretty nice.

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import uwebsockets.client
 from mac import get_mac
 
 ID_DEVICE = 1
-WORK_PIN = 18
+SWITCH = [18]
+ACTOR = [19]
 
 try:
     f = open('mac')
@@ -21,7 +22,7 @@ websocket = False
 while not websocket:
     try:
         websocket = uwebsockets.client.connect(
-            'ws://192.168.0.110:8000/ws/device/{}/'.format(ID_DEVICE))
+            'ws://192.168.0.113:8000/ws/device/{}/'.format(ID_DEVICE))
     except OSError:
         websocket = False
 
@@ -33,27 +34,36 @@ if mac is None:
     message = {'mac': mac}
     websocket.send(json.dumps(message))
 
+# use list compreheension
+switches = [Pin(SWITCH[0], Pin.IN, Pin.PULL_UP)]
+actors = [Pin(ACTOR[0], Pin.OUT)]
+
 
 # Interrupt handler
-def toggle_mode(toggler):
-    print(toggler.value())
-    message = {'state': str(toggler.value())}
-    websocket.send(json.dumps(message))
+def toggle_mode_0(toggler):  # Change de name to port specific name
+    if toggler.value() != actors[0].value():
+        print("Interrupção! Valor do pino:", toggler.value())
+        actors[0].value(toggler.value())
+
+        message = {'state': str(toggler.value())}
+        websocket.send(json.dumps(message))
 
 
-work_pin = Pin(WORK_PIN, Pin.IN, Pin.PULL_UP)
-work_pin.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=toggle_mode)
+switches[0].irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+                handler=toggle_mode_0)
 
 
 def wait_4_messages():
     while True:
         message = websocket.recv()
         try:
-            state = json.loads(message)['state']
+            state = int(json.loads(message)['state'])
+            if state != int(actors[0].value()):
+                print("Message (from ws):", state)
+                print("Pin value:", actors[0].value())
+                actors[0].value(state)
         except TypeError:  # needed to avoid None return while interrupt handler is working
-            state = work_pin.value()
-        print("Message:", state)
-        work_pin.value(state)
+            pass
 
 
 wait_4_messages()
